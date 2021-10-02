80613e029996539aa9ab1d8bc4faf8a061d184e7
AdnanMahmud5040 <90022891+AdnanMahmud5040@users.noreply.github.com>
2021-10-02 15:42:18 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+new WOW().init();
+
+
+var typed = new Typed('.type', {
+        strings: [
+
+            "test run",
+            "responsive website",
+            "responsive theme with a a clean and minimal look."
+        ],
+        typeSpeed: 30,
+        backSpeed: 30,
+        loop : true,
+
+    });
+
+
+
